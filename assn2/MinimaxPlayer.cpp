e1ffc1ee93742ea174cdd89ae84882894f340b35
Tarren Engberg <tarren@outlook.com>
2018-04-29 18:09:30 -0700

    needed functions outlined.

--- assn2/MinimaxPlayer.cpp
+++ assn2/MinimaxPlayer.cpp
@@ -10,6 +10,47 @@
 
 using std::vector;
 
+
+
+/* Start: Tarren's functions */
+
+playerSymbol player(state) { // whose turn it is
+
+}
+
+state results(state, action){ // the board state after 'action' is taken on 'state'.
+
+}
+
+boolean terminal(state) { // returns true if 'state' is a terminal state. 
+
+}
+
+successors(state) { // (AKA 'actions') needs to return an set of legal board states. There should already be a function for state legality.
+
+}
+
+int utility(state, player) { 
+
+}
+
+minimax(state) {
+	if(terminal(state)){
+		return utility(state);
+	}
+	if(player(state) == MAX){
+		// take the minimax of each child and return the smallest.
+	}
+	if(player(state) == MIN){
+		// take the minimax of each child and return the largest.
+	}
+}
+
+
+/* End: Tarren's functions */
+
+
+
 MinimaxPlayer::MinimaxPlayer(char symb) :
 		Player(symb) {
 
