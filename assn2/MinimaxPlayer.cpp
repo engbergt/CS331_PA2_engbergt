caa30ad3617d36e04759d26b9ce3c743deb37b22
Tarren Engberg <tarren@outlook.com>
2018-05-03 18:18:53 -0700

    added action struct to keep track of row and col of each state producted by the successors function.

--- assn2/MinimaxPlayer.cpp
+++ assn2/MinimaxPlayer.cpp
@@ -3,7 +3,10 @@
  *
  *  Created on: Apr 17, 2015
  *      Author: wong
+ * 	Ammended on : May 4th, 2018
+ * 		Author: engbergt
  */
+
 #include <iostream>
 #include <assert.h>
 #include <vector>
@@ -11,15 +14,17 @@
 #include "MinimaxPlayer.h"
 
 
-using std::vector;
-
-
-
 /* Start: Tarren's functions */
 
+struct action = {
+	int column;
+	int row;
+	OthelloBoard resultantBoard;
+};
 
-bool terminal(OthelloBoard state) { // returns true if 'state' is a terminal state. 
-	if(state::has_legal_moves_remaining(state::get_p1_symbol()) || state::has_legal_moves_remaining(state::get_p2_symbol())){
+
+bool terminal(OthelloBoard *state) { // returns true if 'state' is a terminal state. 
+	if(state->has_legal_moves_remaining(state->get_p1_symbol()) || state->has_legal_moves_remaining(state->get_p2_symbol())){
 		return false;
 	}
 	else {
@@ -27,53 +32,58 @@ bool terminal(OthelloBoard state) { // returns true if 'state' is a terminal sta
 	}
 }
 
-std::vector<OthelloBoard> successors(OthelloBoard state) { // (AKA 'actions') needs to return an set of legal board states. There should already be a function for state legality.
+std::vector<OthelloBoard> successors(OthelloBoard *state, char playerSymbol) { // (AKA 'actions') needs to return an set of legal board states. There should already be a function for state legality.
 
-	std::vector<OthelloBoard> validSuccessors;
+	std::vector<action> validSuccessors;
 
-	for(int c = 0; c < state::get_num_cols(); c++){
-		for(int r = 0; c < state::get_num_rows(); r++){
-			if(state::is_cell_empty(c,r) && state::is_legal_move(c,r,????)){
-				OthelloBoard tempBoard = state; // Clone the board ?? Ask Glen.
-				tempBoard::play_move(c,r,????) // Add a piece to this c,r space, on the clone.
-				
-				validSuccessors.push_back(state); // Push clone to the possible successors 
+	for(int c = 0; c < state->get_num_cols(); c++){
+		for(int r = 0; c < state->get_num_rows(); r++){
+			if(state->is_cell_empty(c,r) && state->is_legal_move(c, r, playerSymbol)){
+				action tempAction;
+				tempAction.resultantBoard = state; // Clone the board ?? Ask Glen.
+				tempAction.resultantBoard->play_move(c, r, playerSymbol)
+				tempAction.column = c;
+				tempAction.row = r;
+				validSuccessors.push_back(tempAction);
+				// OthelloBoard tempBoard = state; // Clone the board ?? Ask Glen.
+				// tempBoard->play_move(c,r,????) // Add a piece to this c,r space, on the clone.
+				// validSuccessors.push_back(state); // Push clone to the possible successors 
 			}
 		}
 	}
 }
 
-int utility(OthelloBoard state) { 
-	int p1score = state::count_score(state::get_p1_symbol);
-	int p2score = state::count_score(state::get_p2_symbol);
+int utility(OthelloBoard *state) { 
+	int p1score = state->count_score(state->get_p1_symbol());
+	int p2score = state->count_score(state->get_p2_symbol());
 	return p1score - p2score; // the utility of a board state is the max players on board points subtract the min players on board points.
 }
 
-int maxValue(OthelloBoard state){
+int maxValue(OthelloBoard *state){
 	if(terminal(state)){
 		return utility(state);
 	}
-	value = -INT16_MAX; // negative infinity.
-	for(int& i: successors(state)) {
+	value = -INT16_MAX; // negative "infinity".
+	for(int& i: successors(state, state->get_p1_symbol())) { // I may have this player symbol backwards
 		value = std::max(value, minValue(state));
 	}
 }
 
-int minValue(OthelloBoard state){
+int minValue(OthelloBoard *state){
 	if(terminal(state)){
 		return utility(state);
 	}
-	value = +INT16_MAX; // positive infinity.
-	for(int& i: successors(state)) {
+	value = +INT16_MAX; // positive "infinity".
+	for(int& i: successors(state, state->get_p2_symbol())) { // I may have this player symbol backwards
 		value = std::min(value, minValue(state));
 	}
 }
 
-OthelloBoard minimax(OthelloBoard state) {
+OthelloBoard minimax(OthelloBoard *state) {
 
 	int value = maxValue(state);
-	return successors(state)[value];
-
+	successors(state)[value]; // not right.. we need to return an action.. ?
+	return 
 }
 
 
